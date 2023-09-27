cc41a65498cbe91b65d28a098a12aeeb2d3dd198
OdinsPlasmaRifle <generalaetius.jbvb@gmail.com>
2023-09-27 17:38:48 +0200

    Added more UX updates, improved jumping

--- world.gd
+++ world.gd
@@ -3,16 +3,26 @@ extends Node2D
 @export var next_level: PackedScene
 
 @onready var level_completed = $CanvasLayer/LevelCompleted
+@onready var start_in = %StartIn
+@onready var start_in_label = %StartInLabel
+@onready var animation_player = $AnimationPlayer
 
 func _ready():
-	RenderingServer.set_default_clear_color(Color.BLACK)
 	Events.level_completed.connect(show_level_completed)
+	get_tree().paused = true
+	start_in.visible = true
+	LevelTransition.fade_from_black()
+	animation_player.play("countdown")
+	await animation_player.animation_finished
+	get_tree().paused = false
+	start_in.visible = false
 	
 func show_level_completed():
 	level_completed.show()
-	if not next_level is PackedScene: return
 	get_tree().paused = true
+	await get_tree().create_timer(1.0).timeout
+	if not next_level is PackedScene: return
 	await LevelTransition.fade_to_black()
 	get_tree().paused = false
 	get_tree().change_scene_to_packed(next_level)
-	LevelTransition.fade_from_black()
+	
